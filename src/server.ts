2b322fc27b3dd468864a697fb1de7f0c0d8992d0
Joao Gomes <joaopfg.2002@gmail.com>
2022-05-11 14:56:34 +0100

    Update migrations and port

--- src/server.ts
+++ src/server.ts
@@ -8,5 +8,5 @@ app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 app.use(routes);
 app.listen(process.env.PORT || 3333, () => {
-  console.log("Server running on port 3333");
+  console.log("Server running!");
 });
