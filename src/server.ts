319c1cf9fc90f7d620851a3369eabfb307d083e2
Joao Gomes <joaopfg.2002@gmail.com>
2022-05-11 14:40:13 +0100

    Setup project for deploy -> add port

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,6 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 app.use(routes);
-app.listen(3333, () => {
+app.listen(process.env.PORT || 3333, () => {
   console.log("Server running on port 3333");
 });
